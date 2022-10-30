11fc14c48ace4c60d0ffe6f2fd8020b19bc7ceef
codEnjoyer <ch4shkin.nikita@gmail.com>
2022-10-30 21:50:50 +0500

    Refactored 4.2

--- 1.5 Elements of a functional programming/4.2 Table_filtration.py
+++ 1.5 Elements of a functional programming/4.2 Table_filtration.py
@@ -1,38 +1,43 @@
-from csv import reader
+from csv import reader as csv_reader
 from re import sub
 from enum import Enum
 from prettytable import PrettyTable, ALL
 
 
+def custom_quit(msg: str) -> None:
+    print(msg)
+    quit()
+
+
 class Translate(Enum):
-    name = 'Название'
-    description = 'Описание'
-    key_skills = 'Навыки'
-    experience_id = 'Опыт работы'
-    premium = 'Премиум-вакансия'
-    employer_name = 'Компания'
-    salary_from = 'Нижняя граница вилки оклада'
-    salary_to = 'Верхняя граница вилки оклада'
-    salary_gross = 'Оклад указан до вычета налогов'
-    salary_currency = 'Идентификатор валюты оклада'
-    salary = 'Оклад'
-    area_name = 'Название региона'
-    published_at = 'Дата публикации вакансии'
-    AZN = "Манаты"
-    BYR = "Белорусские рубли"
-    EUR = "Евро"
-    GEL = "Грузинский лари"
-    KGS = "Киргизский сом"
-    KZT = "Тенге"
-    RUR = "Рубли"
-    UAH = "Гривны"
-    USD = "Доллары"
-    UZS = "Узбекский сум"
-    noExperience = "Нет опыта"
-    between1And3 = "От 1 года до 3 лет"
-    between3And6 = "От 3 до 6 лет"
-    moreThan6 = "Более 6 лет"
-    dict_naming_reverse: (str, str) = {'Название': 'name',
+    name: str = 'Название'
+    description: str = 'Описание'
+    key_skills: str = 'Навыки'
+    experience_id: str = 'Опыт работы'
+    premium: str = 'Премиум-вакансия'
+    employer_name: str = 'Компания'
+    salary_from: str = 'Нижняя граница вилки оклада'
+    salary_to: str = 'Верхняя граница вилки оклада'
+    salary_gross: str = 'Оклад указан до вычета налогов'
+    salary_currency: str = 'Идентификатор валюты оклада'
+    salary: str = 'Оклад'
+    area_name: str = 'Название региона'
+    published_at: str = 'Дата публикации вакансии'
+    AZN: str = "Манаты"
+    BYR: str = "Белорусские рубли"
+    EUR: str = "Евро"
+    GEL: str = "Грузинский лари"
+    KGS: str = "Киргизский сом"
+    KZT: str = "Тенге"
+    RUR: str = "Рубли"
+    UAH: str = "Гривны"
+    USD: str = "Доллары"
+    UZS: str = "Узбекский сум"
+    noExperience: str = "Нет опыта"
+    between1And3: str = "От 1 года до 3 лет"
+    between3And6: str = "От 3 до 6 лет"
+    moreThan6: str = "Более 6 лет"
+    dict_naming_reverse: {str, str} = {'Название': 'name',
                                        'Описание': 'description',
                                        'Навыки': 'key_skills',
                                        'Опыт работы': 'experience_id',
@@ -47,110 +52,90 @@ class Translate(Enum):
                                        'Дата публикации вакансии': 'published_at'}
 
     @classmethod
-    def translate_to_rus(cls, data: list) -> list:
+    def translate_list_to_rus(cls, data: list) -> list:
         return [cls.__getitem__(name).value for name in data]
 
     @classmethod
-    def translate_to_en(cls, data: list, dict_naming: dict) -> list:
+    def translate_list_to_en(cls, data: list, dict_naming: dict) -> list:
         return [dict_naming[name] for name in data]
 
 
-def process_filter_parameter(row_filter: str) -> str:
-    if row_filter == "":
+class UserInput:
+    def __init__(self):
+        self.file_name = '../vacancies_medium.csv'  # input('Введите название файла: ')  #
+        self.filter_parameter = UserInput.process_filter_parameter(input('Введите параметр фильтрации: '))
+        self.rows = input('Введите диапазон вывода: ').split()
+        self.fields = input('Введите требуемые столбцы: ').split(', ')
+
+    @staticmethod
+    def process_filter_parameter(row_filter: str) -> str:
+        valid_keys = ["Название", "Навыки", "Опыт работы", "Премиум-вакансия", "Компания", "Оклад",
+                      "Идентификатор валюты оклада", "Название региона", "Дата публикации вакансии"]
+
+        if row_filter != "" and ": " not in row_filter:
+            custom_quit("Формат ввода некорректен")
+
+        if row_filter != "" and row_filter.split(": ")[0] not in valid_keys:
+            custom_quit("Параметр поиска некорректен")
+
         return row_filter
 
-    acceptable = ["Название", "Навыки", "Опыт работы", "Премиум-вакансия", "Компания", "Оклад",
-                  "Идентификатор валюты оклада", "Название региона", "Дата публикации вакансии"]
 
-    if ": " not in row_filter:
-        print("Формат ввода некорректен")
-        quit()
+class CSV:
+    data: csv_reader
+    title: list
+    rows: list
 
-    if row_filter.split(": ")[0] not in acceptable:
-        print("Параметр поиска некорректен")
-        quit()
+    def __init__(self, file_name: str):
+        with open(file_name, 'r', newline='', encoding='utf-8-sig') as file:
+            self.data = csv_reader(file)
+            try:
+                self.title = next(self.data)
+            except StopIteration:
+                custom_quit('Пустой файл')
 
-    return row_filter
+            self.rows = [row for row in self.data
+                         if len(list(filter(lambda word: word != '', row))) == len(self.title)]
 
-
-def process_user_input() -> dict:
-    file_name = input('Введите название файла: ')  # '../vacancies_medium.csv'  #
-    filter_parameter = process_filter_parameter(input('Введите параметр фильтрации: '))
-    rows = input('Введите диапазон вывода: ').split()
-    fields = input('Введите требуемые столбцы: ').split(', ')
-    return {'file_name': file_name,
-            'filter_parameter': filter_parameter,
-            'rows': rows,
-            'fields': fields}
-
-
-def csv_reader(file_name: str) -> (list, list):
-    with open(file_name, 'r', newline='', encoding='utf-8-sig') as file:
-        data = reader(file)
-        try:
-            header = next(data)
-        except StopIteration:
-            print("Пустой файл")
-            quit()
-
-        rows = [row for row in data
-                if len(list(filter(lambda word: word != '', row))) == len(header)]
-        if len(rows) == 0:
-            print("Нет данных")
-            quit()
-
-    return header, rows
-
-
-def parse_row_vacancy(row_vacs: list) -> dict:
-    temp_dict = {}
-    for index, value in enumerate(title):
-        temp_dict[value] = parse_html(row_vacs[index])
-    return temp_dict
-
-
-def parse_html(line: str) -> str:
-    line = sub('<.*?>', '', line)
-    line = line.replace("\r\n", "\n")
-    res = [' '.join(word.split()) for word in line.split('\n')]
-    return res[0] if len(res) == 1 else res  # Спасибо Яндекс.Контесту за еще один костыль!
-
-
-def format_vacancy_field(key: str, value: str or list) -> str:
-    if key == 'key_skills' and type(value) == list:
-        return "\n".join(value)
-    elif value in ['True', 'False']:
-        return value.replace('True', 'Да').replace('False', 'Нет')
-    elif key in ['experience_id', 'salary_currency']:
-        return Translate.__getitem__(value).value
-    elif key in ['salary_from', 'salary_to']:
-        return f"{int(float(value)):,}".replace(',', ' ')
-    elif key == 'published_at':
-        return '.'.join(reversed(value[:10].split('-')))
-    else:
-        return value
+            if len(self.rows) == 0:
+                custom_quit('Нет данных')
 
 
 class Vacancy:
-    def __init__(self, fields: dict):
-        self.name = None
-        self.description = None
-        self.key_skills = None
-        self.experience_id = None
-        self.premium = None
-        self.employer_name = None
-        self.salary_from = None
-        self.salary_to = None
-        self.salary_gross = None
-        self.salary_currency = None
-        self.area_name = None
-        self.published_at = None
-        self.salary = None
+    name: str
+    description: str
+    key_skills: str or list
+    experience_id: str
+    premium: str
+    employer_name: str
+    salary_from: int or float
+    salary_to: int or float
+    salary_gross: str
+    salary_currency: str
+    area_name: str
+    published_at: str
+    salary: str
 
+    def __init__(self, fields: dict):
         for key, value in fields.items():
-            self.__setattr__(key, format_vacancy_field(key, value))
+            self.__setattr__(key, Vacancy.get_correct_field(key, value))
 
         self.set_salary()
+
+    @staticmethod
+    def get_correct_field(key: str, value: str or list) -> str:
+        if key == 'key_skills' and type(value) == list:
+            return "\n".join(value)
+        elif value in ['True', 'False']:
+            return value.replace('True', 'Да').replace('False', 'Нет')
+        elif key in ['experience_id', 'salary_currency']:
+            return Translate.__getitem__(value).value
+        elif key in ['salary_from', 'salary_to']:
+            return f"{int(float(value)):,}".replace(',', ' ')
+        elif key == 'published_at':
+            return '.'.join(reversed(value[:10].split('-')))
+        else:
+            return value
 
     def set_salary(self):
         s_gross = 'Без вычета налогов' if self.salary_gross == 'Да' else 'С вычетом налогов'
@@ -174,54 +159,77 @@ class Vacancy:
             return self.__dict__[Translate.__getitem__('dict_naming_reverse').value[f_key]] == f_value
 
 
-def configure_rus_field_names(tab: PrettyTable, fields: list, skip: list) -> None:
-    res = ['№']
-    for field in fields:
-        if field in skip:
-            continue
-        if field == 'salary_currency':
-            field = 'salary'
-        res.append(Translate.__getitem__(field).value)
-    tab.field_names = res
+class Table:
+    table: PrettyTable
+
+    def __init__(self):
+        self.table = PrettyTable(hrules=ALL, align='l')
+
+    def set_rus_fields(self, fields: list, skip: list) -> None:
+        res = ['№']
+        for field in fields:
+            if field in skip:
+                continue
+            if field == 'salary_currency':
+                field = 'salary'
+            res.append(Translate.__getitem__(field).value)
+        self.table.field_names = res
+
+    def configure(self, fields: list) -> None:
+        self.set_rus_fields(fields, skip=['salary_from', 'salary_to', 'salary_gross'])
+        self.table.max_width = 20
+        self.table.custom_format = (lambda f, v: f"{v}"[:100] + '...' if len(str(v)) > 100 else f"{v}"[:100])
+
+    def fill(self, vacs: list):
+        for index, vac in enumerate(vacs):
+            res = [index + 1]
+            for t in self.table.field_names[1:]:
+                res.append(vac.__dict__[Translate.__getitem__('dict_naming_reverse').value[t]])
+            self.table.add_row(res)
+
+    def print(self, start: int, end: int, fields: list, sort_by: str = ''):
+        print(self.table.get_string(start=start, end=end, fields=fields, sort_by=sort_by))
 
 
-def configure_table(tab: PrettyTable, fields: list) -> None:
-    configure_rus_field_names(tab, fields, skip=['salary_from', 'salary_to', 'salary_gross'])
-    tab.max_width = 20
-    tab.custom_format = (lambda f, v: f"{v}"[:100] + '...' if len(str(v)) > 100 else f"{v}"[:100])
+def parse_html(line: str) -> str:
+    line = sub('<.*?>', '', line)
+    line = line.replace("\r\n", "\n")
+    res = [' '.join(word.split()) for word in line.split('\n')]
+    return res[0] if len(res) == 1 else res  # Спасибо Яндекс.Контесту за еще один костыль!
 
 
-def fill_table(tab: PrettyTable, vacs: list):
-    for index, vac in enumerate(vacs):
-        res = [index + 1]
-        for t in tab.field_names[1:]:
-            res.append(vac.__dict__[Translate.__getitem__('dict_naming_reverse').value[t]])
-        tab.add_row(res)
+def parse_row_vacancy(row_vacs: list) -> dict:
+    temp_dict = {}
+    for index, value in enumerate(title):
+        temp_dict[value] = parse_html(row_vacs[index])
+    return temp_dict
+
+
+def get_filtered_vacancies(row_vacs: list, filter_parameter: str) -> list:
+    vacs = []
+    for row_vac in row_vacs:
+        parsed_vac = Vacancy(parse_row_vacancy(row_vac))
+        if parsed_vac.pass_filtering(filter_parameter):
+            vacs.append(parsed_vac)
+    return vacs
 
 
 def print_vacancies(vacs: list, rows_slice: list, user_fields: list) -> None:
     if len(vacs) == 0:
-        print('Ничего не найдено')
-        quit()
+        custom_quit('Ничего не найдено')
 
     start = int(rows_slice[0]) - 1 if len(rows_slice) >= 1 else 0
     end = int(rows_slice[1]) - 1 if len(rows_slice) >= 2 else len(vacs)
     fields = ['№', *user_fields] if '' not in user_fields else ''
 
-    table = PrettyTable(hrules=ALL, align='l')
-    configure_table(table, title)
-    fill_table(table, vacs)
+    table = Table()
+    table.configure(title)
+    table.fill(vacs)
+    table.print(start=start, end=end, fields=fields)
 
-    print(table.get_string(start=start, end=end, fields=fields))
 
-
-user_input = process_user_input()
-(title, row_vacancies) = csv_reader(user_input['file_name'])
-
-vacancies = []
-for row_vacancy in row_vacancies:
-    parsed_vac = Vacancy(parse_row_vacancy(row_vacancy))
-    if parsed_vac.pass_filtering(user_input['filter_parameter']):
-        vacancies.append(parsed_vac)
-
-print_vacancies(vacancies, user_input['rows'], user_input['fields'])
+u_i = UserInput()
+csv = CSV(u_i.file_name)
+(title, row_vacancies) = csv.title, csv.rows
+vacancies = get_filtered_vacancies(row_vacancies, u_i.filter_parameter)
+print_vacancies(vacancies, u_i.rows, u_i.fields)
